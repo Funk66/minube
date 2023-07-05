ba33f74e785ebc0c251c2706eea153108eef9125
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2023-07-05 08:41:37 +0200

    Fix misconfiguration of server

--- assets/userdata.sh
+++ assets/userdata.sh
@@ -3,19 +3,44 @@
 apt update
 apt upgrade -y
 apt install -y awscli
+hostnamectl set-hostname minube
+
+INET=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+mkdir /etc/pihole
+cat << EOF > /etc/pihole/setupVars.conf
+WEBPASSWORD=
+PIHOLE_INTERFACE=wg0
+IPV4_ADDRESS=$INET/24
+QUERY_LOGGING=true
+INSTALL_WEB=true
+DNSMASQ_LISTENING=local
+PIHOLE_DNS_1=1.1.1.1
+PIHOLE_DNS_2=8.8.8.8
+DNS_FQDN_REQUIRED=true
+DNS_BOGUS_PRIV=true
+DNSSEC=true
+TEMPERATUREUNIT=C
+WEBUIBOXEDLAYOUT=traditional
+API_QUERY_LOG_SHOW=all
+API_PRIVACY_MODE=false
+EOF
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git /tmp/pi-hole
+/tmp/pi-hole/automated\ install/basic-install.sh --unattended
+aws s3 cp s3://minube-backups/etc/hosts /etc/hosts
+aws s3 cp s3://minube-backups/etc/pihole/pihole-FTL.db /etc/pihole/pihole-FTL.db
+echo "https://blocklistproject.github.io/Lists/everything.txt" >> /etc/pihole/adlists.list
 
 cat << EOF > /tmp/pivpn.conf
 IPv4dev=ens5
 install_user=ubuntu
 install_home=/home/ubuntu
 VPN=wireguard
-pivpnNET=10.0.0.0
+pivpnNET="10.10.10.0/24"
 subnetClass=24
-ALLOWED_IPS="10.0.0.0/28"
+ALLOWED_IPS="10.10.10.0/24"
 pivpnMTU=1420
 pivpnPORT=51820
-pivpnDNS1=169.254.169.253
-pivpnDNS2=1.1.1.1
+pivpnDNS1=$INET
 pivpnHOST=minube.guirao.com
 pivpnPERSISTENTKEEPALIVE=25
 UNATTUPG=1
@@ -24,6 +49,6 @@ EOF
 git clone https://github.com/pivpn/pivpn /tmp/pivpn
 cd /tmp/ || exit
 bash pivpn/auto_install/install.sh --unattended /tmp/pivpn.conf
-aws s3 cp s3://minube-backups/pivpn.tgz - | tar -C /etc/wireguard -xzf -
+aws s3 cp s3://minube-backups/etc/wireguard.tgz - | tar -C /etc/wireguard -xzf -
 
 reboot now
