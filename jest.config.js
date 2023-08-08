66ed123be85d4d587efefcc174a15dd718040b35
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2023-08-08 08:25:29 +0200

    Fix linting errors

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('jest').Config} */
 /*
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
 
-module.exports = {
+export default {
   
   // All imported modules in your tests should be mocked automatically
   // automock: false,
