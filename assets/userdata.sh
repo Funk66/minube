ce364548b0f9928526cf5fa53947cdfcc84a00eb
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2024-03-29 16:28:57 +0100

    Change Caddy to the stable branch

--- assets/userdata.sh
+++ assets/userdata.sh
@@ -17,8 +17,8 @@ echo \
   \"$(. /etc/os-release && echo "$VERSION_CODENAME")\" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/testing/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-testing-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/testing/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-testing.list
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 
 apt update
 apt upgrade -y
