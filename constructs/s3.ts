d3d8a30244817b2ee418ceaea9f14933f6182002
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2024-05-11 08:37:05 +0200

    Add versioning to all buckets

--- constructs/s3.ts
+++ constructs/s3.ts
@@ -4,59 +4,39 @@ import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket";
 import { S3Object } from "@cdktf/provider-aws/lib/s3-object";
 import { S3BucketVersioningA } from "@cdktf/provider-aws/lib/s3-bucket-versioning";
 import { S3BucketLifecycleConfiguration } from "@cdktf/provider-aws/lib/s3-bucket-lifecycle-configuration";
-import { S3BucketServerSideEncryptionConfigurationA } from "@cdktf/provider-aws/lib/s3-bucket-server-side-encryption-configuration";
 
 const assets = path.resolve(__dirname, "../assets");
 
-export class Buckets extends Construct {
-  public readonly backups: S3Bucket;
-  public readonly storage: S3Bucket;
-  public readonly photos: S3Bucket;
+export class S3 extends Construct {
+  public readonly buckets: Record<string, S3Bucket> = {};
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    this.backups = new S3Bucket(this, "minube-backups", {
-      bucket: "minube-backups",
-    });
+    for (const name of ["backups", "storage", "photos"]) {
+      this.buckets[name] = new S3Bucket(this, `minube-${name}`, {
+        bucket: `minube-${name}`,
+      });
 
-    this.storage = new S3Bucket(this, "minube-storage", {
-      bucket: "minube-storage",
-    });
-
-    this.photos = new S3Bucket(this, "minube-photos", {
-      bucket: "minube-photos",
-    });
-
-    new S3BucketVersioningA(this, "backups-versioning", {
-      bucket: this.backups.bucket,
-      versioningConfiguration: {
-        status: "Enabled",
-      },
-    });
-
-    new S3BucketLifecycleConfiguration(this, "backups-lifecycle", {
-      bucket: this.backups.bucket,
-      rule: [
-        {
-          id: "Trashcan",
+      new S3BucketVersioningA(this, `${name}-versioning`, {
+        bucket: this.buckets[name].bucket,
+        versioningConfiguration: {
           status: "Enabled",
-          abortIncompleteMultipartUpload: { daysAfterInitiation: 7 },
-          noncurrentVersionExpiration: { noncurrentDays: 90 },
         },
-      ],
-    });
+      });
 
-    new S3BucketServerSideEncryptionConfigurationA(this, "backups-encryption", {
-      bucket: this.photos.bucket,
-      rule: [
-        {
-          applyServerSideEncryptionByDefault: {
-            sseAlgorithm: "AES256",
+      new S3BucketLifecycleConfiguration(this, `${name}-lifecycle`, {
+        bucket: this.buckets[name].bucket,
+        rule: [
+          {
+            id: "Trashcan",
+            status: "Enabled",
+            abortIncompleteMultipartUpload: { daysAfterInitiation: 7 },
+            noncurrentVersionExpiration: { noncurrentDays: 90 },
           },
-        },
-      ],
-    });
+        ],
+      });
+    }
 
     this.backupFile(scope, "/etc/pihole/backup");
     this.backupFile(scope, "/etc/systemd/system/backup.service");
@@ -67,7 +47,7 @@ export class Buckets extends Construct {
   private backupFile(scope: Construct, filepath: string) {
     const filename = path.basename(filepath);
     return new S3Object(scope, filename, {
-      bucket: this.backups.bucket,
+      bucket: this.buckets["backups"].bucket,
       key: filepath,
       source: `${assets}/${filename}`,
       storageClass: "GLACIER_IR",
--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Construct } from "constructs";
 import { App, TerraformStack, CloudBackend, NamedCloudWorkspace } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { VPC } from "./constructs/vpc";
-import { Buckets } from "./constructs/s3";
+import { S3 } from "./constructs/s3";
 import { Role } from "./constructs/iam";
 import { EC2 } from "./constructs/ec2";
 import { DNS } from "./constructs/dns";
@@ -14,18 +14,18 @@ class Minube extends TerraformStack {
     new AwsProvider(this, "aws", { region: "eu-central-1" });
 
     const dns = new DNS(this, "dns");
-    const buckets = new Buckets(this, "buckets");
+    const s3 = new S3(this, "buckets");
     const role = new Role(this, "role", {
-      backups: buckets.backups,
-      photos: buckets.photos,
+      backups: s3.buckets.backups,
+      photos: s3.buckets.photos,
       hostedZone: dns.zone.arn,
     });
     const vpc = new VPC(this, "vpc");
     new EC2(this, "ec2", {
       vpc: vpc,
       role: role,
-      backups: buckets.backups,
-      photos: buckets.photos,
+      backups: s3.buckets.backups,
+      photos: s3.buckets.photos,
     });
   }
 }
