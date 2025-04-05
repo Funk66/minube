2a07e25c158257c77b35091faba880a4b687fc87
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2025-04-05 23:24:22 +0200

    Add quadlets

--- assets/userdata.sh
+++ assets/userdata.sh
@@ -3,22 +3,17 @@
 set -eux -o pipefail
 
 aws() {
-	podman run --rm -v "/home:/home" -v "/etc:/etc" -e AWS_USE_DUALSTACK=true amazon/aws-cli "$@"
+	podman run --rm -v "/home:/home" -v "/etc:/etc" -e AWS_USE_DUALSTACK=true docker.io/amazon/aws-cli "$@"
 }
 
 export DEBIAN_FRONTEND=noninteractive
-
-mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg >/dev/null
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
 
 apt update
 apt upgrade -y
 
 install -m 0755 -d /etc/apt/keyrings
 
-apt install -y unzip sqlite3 debian-keyring debian-archive-keyring apt-transport-https nginx libnginx-mod-stream python3-certbot-dns-route53 alloy fail2ban podman podman-compose
-hostnamectl set-hostname minube
+apt install -y podman
 
 fallocate -l 2G /swap
 chmod 600 /swap
@@ -27,89 +22,23 @@ swapon /swap
 echo '/swap none swap sw 0 0' >>/etc/fstab
 echo 'vm.swappiness=10' >>/etc/sysctl.conf
 
+sed -r -i 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+rm /etc/resolv.conf
+ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl restart systemd-resolved
+loginctl enable-linger ubuntu
+mkdir -p ~/.config/containers/systemd/
+systemctl --user enable podman-auto-update.service
+
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-INET=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
 INET6=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/ipv6)
 
-mkdir /etc/pihole
-cat <<EOF >/etc/pihole/setupVars.conf
-WEBPASSWORD=
-PIHOLE_INTERFACE=wg0
-IPV4_ADDRESS=$INET/26
-IPV6_ADDRESS=$INET6/64
-QUERY_LOGGING=true
-INSTALL_WEB=true
-DNSMASQ_LISTENING=local
-PIHOLE_DNS_1=193.110.81.9
-PIHOLE_DNS_2=185.253.5.9
-DNS_FQDN_REQUIRED=true
-DNS_BOGUS_PRIV=true
-DNSSEC=true
-TEMPERATUREUNIT=C
-WEBUIBOXEDLAYOUT=traditional
-API_QUERY_LOG_SHOW=all
-API_PRIVACY_MODE=false
-EOF
-cat <<EOF >/etc/pihole/pihole-FTL.conf
-MAXDBDAYS=1425
-DBFILE=/data/pihole/pihole-FTL.db
-EOF
-git clone --quiet --depth 1 https://github.com/pi-hole/pi-hole.git /tmp/pi-hole
-/tmp/pi-hole/automated\ install/basic-install.sh --unattended
-echo "https://blocklistproject.github.io/Lists/everything.txt" >>/etc/pihole/adlists.list
-
-cat <<EOF >/tmp/pivpn.conf
-IPv4dev=ens5
-IPv6dev=ens5
-install_user=ubuntu
-install_home=/home/ubuntu
-VPN=wireguard
-pivpnNET="10.10.10.0"
-subnetClass=26
-pivpnNETv6="fd11:5ee:bad:c0de::"
-subnetClassv6=64
-pivpnforceipv6route=1
-pivpnforceipv6=0
-pivpnenableipv6=1
-ALLOWED_IPS="10.10.10.0/26, fd11:5ee:bad:c0de::/64"
-pivpnMTU=1420
-pivpnPORT=51820
-pivpnDNS1=10.10.10.1
-pivpnHOST=minube.guirao.net
-pivpnPERSISTENTKEEPALIVE=25
-UNATTUPG=1
-EOF
-
-git clone --quiet --depth 1 https://github.com/pivpn/pivpn /usr/local/src/pivpn
-bash /usr/local/src/pivpn/auto_install/install.sh --unattended /tmp/pivpn.conf
-aws s3 cp --recursive s3://minube-backups/etc/ /etc/
-chmod +x /etc/pihole/backup
-ln -s /etc/pihole/backup /etc/cron.daily/backup
-for SERVICE in podman backup wg-quick@casa alloy fail2ban fail2ban_exporter; do
-	systemctl enable "$SERVICE"
-done
-
-HOSTED_ZONE=$(aws route53 list-hosted-zones-by-name --dns-name guirao.net | jq -r '.HostedZones[0].Id')
 for SUBDOMAIN in minube mail calendar docs photos; do
 	aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE" --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'"$SUBDOMAIN"'.guirao.net.","Type":"AAAA","TTL":300,"ResourceRecords":[{"Value":"'"$INET6"'"}]}}]}'
 done
 
-sed -i 's|= 80|= 8053|' /etc/lighttpd/lighttpd.conf
-
-echo "certbot certonly --dns-route53 -m postmaster@guirao.net -d *.guirao.net --agree-tos --non-interactive" >/etc/cron.weekly/certbot
-chmod +x /etc/cron.weekly/certbot
-/etc/cron.weekly/certbot
-
-echo "find /var/log/nginx -type f -mtime +7 -exec rm {} \;" >/etc/cron.daily/cleanlogs
-chmod +x /etc/cron.daily/cleanlogs
-
-curl -L https://gitlab.com/hectorjsmith/fail2ban-prometheus-exporter/-/releases/v0.10.2/downloads/fail2ban_exporter_0.10.2_linux_arm64.tar.gz | tar --wildcards -xz -C /usr/bin 'fail2ban_exporter'
-
 echo 'UUID="bb61a946-053e-432e-96bc-02d07c30a820" /data xfs defaults,nofail' >>/etc/fstab
 mount -a
 echo 'set -o vi' >>/etc/profile
-
-aws s3 cp --recursive s3://minube-backups/home/ /home/
-podman-compose -f /data/immich/docker-compose.yaml up -d
 
 reboot now
