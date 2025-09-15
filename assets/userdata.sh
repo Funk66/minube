1822d5fe2a7e56c4600d978e046d7bc73d9178a4
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2025-09-15 20:39:19 +0200

    Uninstall the SSM agent

--- assets/userdata.sh
+++ assets/userdata.sh
@@ -12,6 +12,8 @@ hostnamectl set-hostname minube
 echo "127.0.0.1 minube" >>/etc/hosts
 sed -i 's/preserve_hostname: false/preserve_hostname: true/' /etc/cloud/cloud.cfg
 
+snap remove amazon-ssm-agent
+
 apt update
 apt upgrade -y
 apt install -y podman
