99f26c7c7c5c98a27e789ebbb042b468caef09c0
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2025-10-08 07:53:37 +0200

    Add bash aliases

--- assets/fs/opt/backup/immich.sh
+++ assets/fs/opt/backup/immich.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 sync() {
-	podman run --rm -v /data/immich:/immich docker.io/amazon/aws-cli s3 sync --no-progress --exclude ".immich" "$@"
+	podman run --rm -v /data/immich:/immich docker.io/amazon/aws-cli s3 sync --no-progress --delete --exclude ".immich" "$@"
 }
 
 sync --storage-class DEEP_ARCHIVE /immich/library s3://minube-photos/library
